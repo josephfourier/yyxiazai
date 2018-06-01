5d56b02c561ace9b185a084e78cd274ea2ca97b0
liuaiping <2779429237@qq.com>
2018-06-01 16:08:41 +0800

    init
    Signed-off-by: liuaiping <2779429237@qq.com>

---
+++ src/main/java/com/yyxiazai/spring/SpringCacheManagerWrapper.java
@@ -0,0 +1,107 @@
+package com.yyxiazai.spring;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.CacheManager;
+import org.springframework.cache.support.SimpleValueWrapper;
+
+import net.sf.ehcache.Ehcache;
+
+public class SpringCacheManagerWrapper implements CacheManager {
+
+	private org.springframework.cache.CacheManager cacheManager;
+	
+	public void setCacheManager(org.springframework.cache.CacheManager cacheManager) {
+		this.cacheManager = cacheManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <K, V> Cache<K, V> getCache(String cacheName) throws CacheException {
+		org.springframework.cache.Cache springCache = cacheManager.getCache(cacheName);
+		return new SpringCacheWrapper(springCache);
+	}
+
+	@SuppressWarnings("rawtypes")
+	static class SpringCacheWrapper implements Cache {
+
+		private org.springframework.cache.Cache springCache;
+		
+		public SpringCacheWrapper(org.springframework.cache.Cache cache) {
+			this.springCache = cache;
+		}
+	
+		@Override
+        public Object get(Object key) throws CacheException {
+            Object value = springCache.get(key);
+            if (value instanceof SimpleValueWrapper) {
+                return ((SimpleValueWrapper) value).get();
+            }
+            return value;
+        }
+
+        @Override
+        public Object put(Object key, Object value) throws CacheException {
+            springCache.put(key, value);
+            return value;
+        }
+
+        @Override
+        public Object remove(Object key) throws CacheException {
+            springCache.evict(key);
+            return null;
+        }
+
+        @Override
+        public void clear() throws CacheException {
+            springCache.clear();
+        }
+
+        @Override
+        public int size() {
+            if(springCache.getNativeCache() instanceof Ehcache) {
+                Ehcache ehcache = (Ehcache) springCache.getNativeCache();
+                return ehcache.getSize();
+            }
+            throw new UnsupportedOperationException("invoke spring cache abstract size method not supported");
+        }
+
+        @Override
+        public Set keys() {
+            if(springCache.getNativeCache() instanceof Ehcache) {
+                Ehcache ehcache = (Ehcache) springCache.getNativeCache();
+                return new HashSet(ehcache.getKeys());
+            }
+            throw new UnsupportedOperationException("invoke spring cache abstract keys method not supported");
+        }
+
+        @Override
+        public Collection values() {
+            if(springCache.getNativeCache() instanceof Ehcache) {
+                Ehcache ehcache = (Ehcache) springCache.getNativeCache();
+                List keys = ehcache.getKeys();
+                if (!CollectionUtils.isEmpty(keys)) {
+                    List values = new ArrayList(keys.size());
+                    for (Object key : keys) {
+                        Object value = get(key);
+                        if (value != null) {
+                            values.add(value);
+                        }
+                    }
+                    return Collections.unmodifiableList(values);
+                } else {
+                    return Collections.emptyList();
+                }
+            }
+            throw new UnsupportedOperationException("invoke spring cache abstract values method not supported");
+        }
+	}
+}
---
+++ src/main/java/com/yyxiazai/spring/SpringUtils.java
@@ -0,0 +1,48 @@
+package com.yyxiazai.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class SpringUtils implements BeanFactoryPostProcessor {
+
+    private static ConfigurableListableBeanFactory beanFactory; // SpringӦ�������Ļ���
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringUtils.beanFactory = beanFactory;
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) throws BeansException {
+        return (T) beanFactory.getBean(name);
+    }
+
+
+    public static <T> T getBean(Class<T> clz) throws BeansException {
+        @SuppressWarnings("unchecked")
+        T result = (T) beanFactory.getBean(clz);
+        return result;
+    }
+
+    public static boolean containsBean(String name) {
+        return beanFactory.containsBean(name);
+    }
+
+
+    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.isSingleton(name);
+    }
+
+
+    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.getType(name);
+    }
+
+
+    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.getAliases(name);
+    }
+}
